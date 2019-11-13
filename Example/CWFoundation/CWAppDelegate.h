af3fb112d71181de29250fc1b5b448f9464ebfa3
chenwu <chenwu@roobo.com>
2019-11-13 15:37:38 +0800

    Initial commit

---
+++ Example/CWFoundation/CWAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CWAppDelegate.h
+//  CWFoundation
+//
+//  Created by JackWchen2015 on 11/13/2019.
+//  Copyright (c) 2019 JackWchen2015. All rights reserved.
+//
+
+@import UIKit;
+
+@interface CWAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Example/CWFoundation/CWViewController.h
@@ -0,0 +1,13 @@
+//
+//  CWViewController.h
+//  CWFoundation
+//
+//  Created by JackWchen2015 on 11/13/2019.
+//  Copyright (c) 2019 JackWchen2015. All rights reserved.
+//
+
+@import UIKit;
+
+@interface CWViewController : UIViewController
+
+@end
